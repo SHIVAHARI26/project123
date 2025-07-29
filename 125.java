b8de8167df8f9b763c4aa8e2fc41e7e5af130388
SHIVAHARI26 <shivaharishivahari90@gmail.com>
2025-07-29 09:06:37 +0530

    making changes

---
+++ 125.java
@@ -0,0 +1,9 @@
+public class OneDArrayExample{
+public static void main (String[] args){
+int[]numbers={10,20,30,40,50};
+System.out.println("1D Array Elements");
+for(int i=0;i<numbers.length;i++){
+System.out.println(numbers[i]);
+}
+}
+}
